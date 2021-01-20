6fe9c34b898d53c8ffc371cdc354bb87792b44be
133814250@qq.com <133814250@qq.com>
2021-01-20 10:30:01 +0800

    Wed Jan 20 10:30:01 CST 2021

--- README.md
+++ README.md
@@ -1449,3 +1449,6 @@ which will bring up Hyperf default home page.
 
   
 
+
+  
+
--- app/Controller/Admin/LogsController.php
+++ app/Controller/Admin/LogsController.php
@@ -4,6 +4,7 @@ declare (strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Controller\BaseController;
+use Core\Common\Extend\Helpers\ArrayHelpers;
 use Crypto\Rand;
 use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\AutoController;
@@ -20,6 +21,7 @@ use App\Middleware\AdminAuthMiddleware;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use App\Constants\StatusCode;
 use App\Models\Log;
+use Hyperf\Utils\Coroutine;
 
 /**
  * LogsController
@@ -45,6 +47,13 @@ class LogsController extends BaseController
     private $model;
 
     /**
+     *
+     * @Inject()
+     * @var IpRegion
+     */
+    private $IpRegionModel;
+
+    /**
      * list
      * @return \Psr\Http\Message\ResponseInterface
      *
@@ -53,31 +62,47 @@ class LogsController extends BaseController
     public function list()
     {
         $reqParam = $this->request->all();
-        $query = $this->model->query();
-
+        $where    = []; //额外条件
+        $query    = $this->model->query()->where($where)
+            ->select('id', 'real_ip', 'city_id', 'url', 'uri', 'arguments', 'method', 'device', 'browser', 'execution_time', 'created_at', 'code', 'msg');
         [$querys, $sort, $order, $offset, $limit] = $this->model->buildTableParams($reqParam, $query);
-        $where = []; //额外条件
 
         $total = $querys
             ->where($where)
             ->orderBy($sort, $order)
             ->count();
-        //        Db::enableQueryLog();
+
         $list = $querys
-            ->where($where)
             ->orderBy($sort, $order)
             ->offset($offset)->limit($limit)
-            ->get();
-        //        var_export(Db::getQueryLog());
+            ->get()
+            ->toArray();
 
-        $list = $list ? $list->toArray() : [];
-        if(!empty($list)){
+        if (!empty($list)) {
             foreach ($list as $k => $v) {
-                //                $list[$k]['status'] = $v['status'] === 0 ? false : true;
+                $list[$k]['city_id'] = $this->IpRegionModel->where(['id' => $v['city_id']])->value('name');
             }
             unset($v);
         }
-        $result = array("total" => $total, "rows" => $list);
+
+        var_export(['start' =>date("H:i:s")]);
+        $result = parallel([
+            function () {
+                sleep(3);
+                return 'aaa';
+            },
+            function () {
+                sleep(1);
+                return 'bbb';
+            },
+            function () {
+                sleep(3);
+                return 'ccc';
+            }
+        ]);
+        var_export(['$result' =>$result]);
+        var_export(['end' =>date("H:i:s")]);
+        $result = ["total" => $total, "rows" => $list];
         return $this->success($result);
     }
 
